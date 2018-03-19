a40e28b614c870fda79811d4174726594ea7a5ce
Ji Zhang <ji.zhang@samsung.com>
2018-03-19 13:06:45 +0800

    add TextEditor problem

---
+++ day6/TextEditor/user.cpp
@@ -0,0 +1,23 @@
+//
+// Created by zhangji on 3/19/18.
+//
+
+void init(int N) {
+
+}
+
+void move_cursor(int cmd) {
+
+}
+
+char get_char(int row, int column) {
+
+}
+
+void input_newline() {
+
+}
+
+void input_char(char in_char) {
+    
+}
