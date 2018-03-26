8fb28bc2856b9553082d713c2b5b7ec3253f521c
Ji Zhang <ji.zhang@samsung.com>
2018-03-26 21:47:52 +0800

    update VirusInfection problem's part solution

--- day5/VirusInfection/user.cpp
+++ day5/VirusInfection/user.cpp
@@ -30,6 +30,7 @@ struct Node {
     int total_f_size;
 
     Node *parent;
+    Node *prev_bro;
     Node *next_bro;
     Node *first_chd;
 };
@@ -68,6 +69,7 @@ Node *get_node(int id, int fileSize, Node *parent) {
     node->total_f_cnt = 0;
     node->total_f_size = 0;
     node->parent = parent;
+    node->prev_bro = nullptr;
     node->next_bro = nullptr;
     node->first_chd = nullptr;
     return node;
@@ -101,7 +103,7 @@ Node *find_node(Node *_node, int *pid) {
 }
 
 
-void *update_file_info(Node *node) {
+void *update_file_info_add(Node *node) {
     int tf_size, tf_cnt;
     if (node->type == DR) {
         tf_size = node->total_f_size;
@@ -109,6 +111,23 @@ void *update_file_info(Node *node) {
     } else { // node->type == FL
         tf_size = node->file_size;
         tf_cnt = 1;
+    }
+    Node *p_node = node->parent;
+    while (p_node != nullptr) {
+        p_node->total_f_cnt += tf_cnt;
+        p_node->total_f_size += tf_size;
+        p_node = p_node->parent;
+    }
+}
+
+void *update_file_info_substract(Node *node) {
+    int tf_size, tf_cnt;
+    if (node->type == DR) {
+        tf_size = -node->total_f_size;
+        tf_cnt = -node->total_f_cnt;
+    } else { // node->type == FL
+        tf_size = -node->file_size;
+        tf_cnt = -1;
     }
     Node *p_node = node->parent;
     while (p_node != nullptr) {
@@ -131,20 +150,47 @@ int add(int id, int pid, int fileSize) {
     p_node->first_chd = new_node;
     if (old_first_child != nullptr) {
         new_node->next_bro = old_first_child;
+        old_first_child->prev_bro = new_node;
     }
     //compute total_f_size & total_f_cnt
-    update_file_info(new_node);
+    update_file_info_add(new_node);
     return p_node->total_f_size;
 }
 
 // 2
 int move(int id, int pid) {
-
+    Node *src_node = find_node(&root, &id);
+    // cut down
+    Node *src_parent_n = src_node->parent;
+    Node *src_next_bro = src_node->next_bro;
+    Node *src_prev_bro = src_node->prev_bro;
+    if (src_prev_bro == nullptr) { // src_node is fisrt child
+        src_parent_n->first_chd = src_next_bro;
+    } else {
+        src_prev_bro->next_bro = src_next_bro;
+        src_next_bro->prev_bro = src_prev_bro;
+    }
+    update_file_info_substract(src_node);
+    // move to 
+    Node *tgt_node = find_node(&root, &pid);
+    Node *old_first_child = tgt_node->first_chd;
+    tgt_node->first_chd = src_node;
+    if (old_first_child != nullptr) {
+        src_node->next_bro = old_first_child;
+        old_first_child->prev_bro = src_node;
+    }
+    update_file_info_add(src_node);
 }
 
 //3
 int infect(int id) {
-
+    int total_f_size = root.total_f_size;
+    int total_f_cnt = root.total_f_cnt;
+    int infect_size = total_f_size / total_f_cnt;
+    Node *node = find_node(&root, &id);
+    if (node->type == FL) {
+        
+    }
 }
 
 //4
@@ -154,5 +200,16 @@ int recover(int id) {
 
 //5
 int remove(int id) {
-
+    Node *src_node = find_node(&root, &id);
+    // cut down
+    Node *src_parent_n = src_node->parent;
+    Node *src_next_bro = src_node->next_bro;
+    Node *src_prev_bro = src_node->prev_bro;
+    if (src_prev_bro == nullptr) { // src_node is fisrt child
+        src_parent_n->first_chd = src_next_bro;
+    } else {
+        src_prev_bro->next_bro = src_next_bro;
+        src_next_bro->prev_bro = src_prev_bro;
+    }
+    update_file_info_substract(src_node);
 }
