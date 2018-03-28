091d89edf21eda3a42987ddf7e09296f635d3a61
Ji Zhang <ji.zhang@samsung.com>
2018-03-28 15:40:52 +0800

    add AccountOperation problem

---
+++ AccountOperation/user.cpp
@@ -0,0 +1,21 @@
+//
+// Created by zhangji on 3/28/18.
+//
+
+#define MaxAccount 50000
+
+void Init() {
+}
+
+void NewAccount(char id[11], char password[11], int defaulttime) {
+}
+
+void Logout(char id[11]) {
+}
+
+void Connect(char id[11], char password[11]) {
+}
+
+int Tick() {
+    return 0;
+}
