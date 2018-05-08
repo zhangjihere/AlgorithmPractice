7a0bae9a4e288bb7e70a18dcacad62a75528e4d4
Ji Zhang <ji.zhang@samsung.com>
2018-05-08 10:48:38 +0800

    add problem 20180428-GMemo

---
+++ 20180428-GMemo/user.cpp
@@ -0,0 +1,31 @@
+//
+// Created by zhangji on 5/8/18.
+//
+
+typedef struct {
+    int y, x;
+} AXIS;
+
+void init(int mScreenSize) {
+
+}
+
+void create_memo(int mId, int mY, int mX, int mHeight, int mWidth, char str[]) {
+
+}
+
+AXIS select_memo(int mId) {
+
+}
+
+void move_memo(int mId, int mY, int mX) {
+
+}
+
+void modify_memo(int mId, int mHeight, int mWidth, char str[]) {
+
+}
+
+void get_screen_context(int mY, int mX, char res[5][5]) {
+
+}
