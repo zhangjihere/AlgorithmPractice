2d5e62c364806a9fa09c2568aa4a00a548715763
Ji Zhang <ji.zhang@samsung.com>
2018-06-13 14:21:28 +0800

    update problem 20170512-DiskScheduling

---
+++ 20170512-DiskScheduling/user.cpp
@@ -0,0 +1,38 @@
+/**
+ * zhangji 2018-06-13
+ */
+
+const int MAX = 100000;
+
+void init(int track_size, int head) {
+    // TO DO
+}
+
+void request(int track) {
+    // TO DO
+}
+
+int fcfs() {
+    int track_no = -1;    // TO DO : Need to be changed
+
+    return track_no;
+}
+
+int sstf() {
+    int track_no = -1;    // TO DO : Need to be changed
+
+    return track_no;
+}
+
+int look() {
+    int track_no = -1;    // TO DO : Need to be changed
+
+    return track_no;
+}
+
+int clook() {
+    int track_no = -1;    // TO DO : Need to be changed
+
+    return track_no;
+}
+
